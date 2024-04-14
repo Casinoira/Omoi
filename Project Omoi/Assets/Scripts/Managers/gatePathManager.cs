6df4d326f8049c58614687cf88be4dbbfb5caa83
Casinoira <100073808@students.swinburne.edu.my>
2024-04-14 23:28:21 +0800

    Temporary Pause Menu

--- Project Omoi/Assets/Scripts/Managers/gatePathManager.cs
+++ Project Omoi/Assets/Scripts/Managers/gatePathManager.cs
@@ -6,22 +6,37 @@ public class gatePathManager : MonoBehaviour
 {
     [SerializeField] float lockedAngle1 = 0; 
     [SerializeField] float lockedAngle2 = -58;
-    [SerializeField] int rotationSpeed = 30;
+    [SerializeField] int rotationSpeed = 100;
 
     Vector3 currentEulerAngles;
     public Transform pivotPoint;
-
+    // private bool isRotateLeft = false;
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        if (pivotPoint.eulerAngles.z <= lockedAngle1 ) {
-            currentEulerAngles -= new Vector3(0,0, 2) * Time.deltaTime * rotationSpeed;
-            pivotPoint.localEulerAngles = currentEulerAngles;
+        currentEulerAngles += new Vector3(0,0, 2) * Time.deltaTime * rotationSpeed;
+        pivotPoint.localEulerAngles = currentEulerAngles;
 
-            if (pivotPoint.eulerAngles.z == lockedAngle2) {
-                        currentEulerAngles += new Vector3(0,0, 2) * Time.deltaTime * rotationSpeed;
-                        pivotPoint.localEulerAngles = currentEulerAngles;
-            }
-        }
+        // if (isRotateLeft) {
+        //     currentEulerAngles += new Vector3(0,0, 2) * Time.deltaTime * rotationSpeed;
+        //     pivotPoint.localEulerAngles = currentEulerAngles;
+
+        //     if (pivotPoint.eulerAngles.z == lockedAngle1) {
+        //         isRotateLeft = false;
+        //     }
+        // } else if (!isRotateLeft) {
+        //     currentEulerAngles -= new Vector3(0,0, 2) * Time.deltaTime * rotationSpeed;
+        //     pivotPoint.localEulerAngles = currentEulerAngles;
+
+        //     if (pivotPoint.eulerAngles.z == lockedAngle2) {
+        //         isRotateLeft = true;
+        //     }
+        // }
+    }
+
+    private void LimitRotation() {
+        // pivotPoint.rotation.eulerAngles.z = Mathf.Clamp(pivotPoint.rotation.eulerAngles.z , lockedAngle2, lockedAngle1);
+
+
     }
 }
