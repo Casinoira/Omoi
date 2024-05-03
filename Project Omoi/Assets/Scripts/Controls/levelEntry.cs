b8c0b0ec79d6cbe28ca33b05056aa63aeba71c16
Casinoira <100073808@students.swinburne.edu.my>
2024-05-03 21:44:11 +0800

    Fix Movement Bug

---
+++ Project Omoi/Assets/Scripts/Controls/levelEntry.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class levelEntry : MonoBehaviour
+{
+    public int sceneBuildIndex;
+    public GameObject showButton;
+ 
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.tag == "Player") {
+            showButton.SetActive(true);
+        }
+
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        if(other.tag == "Player") {
+            showButton.SetActive(false);
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D other) {
+        if(other.tag == "Player" && Input.GetKey(KeyCode.E)) {
+            SceneManager.LoadScene(sceneBuildIndex, LoadSceneMode.Single);
+        }
+    }
+}
