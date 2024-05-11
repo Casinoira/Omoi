b17f51cc3fe3170f9139aad00ae7ebf7f9772f11
Casinoira <100073808@students.swinburne.edu.my>
2024-05-12 03:56:14 +0800

    Bug Fix
    - Fix Dragged Blocks still hittable by ball
    - Fix Dragged Blocks stacking on each other
    - Fix Dragged Blocks Not Snapping

--- Project Omoi/Assets/Scripts/Controls/Draggable.cs
+++ Project Omoi/Assets/Scripts/Controls/Draggable.cs
@@ -11,17 +11,27 @@ public class Draggable : MonoBehaviour
     private bool isDragging = false;
     private Vector3 mouseDragStartPosition;
     private Vector3 spriteDragStartPosition;
+
+    private Collider2D collider2D;
+
+    private void Start() {
+        collider2D = GetComponent<Collider2D>();
+    }
     
     public void OnMouseDown() {
         isDragging = true;
         mouseDragStartPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         spriteDragStartPosition = transform.localPosition;
 
+        collider2D.enabled = false;
+
     }
 
     public void OnMouseUp() {
         isDragging = false;
         dragEndedCallback(this);
+        collider2D.enabled = true;
+
     }
 
     void OnMouseDrag() {
--- Project Omoi/Assets/Scripts/Controls/snapController_backup.cs
+++ Project Omoi/Assets/Scripts/Controls/snapController_backup.cs
@@ -2,33 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class snapController : MonoBehaviour
+public class snapController_backup : MonoBehaviour
 {
     public List<Transform> snapPoints;
     public List<Draggable> draggableObjects;
     public float snapRange = 0.5f;
     private Dictionary<Draggable, Vector3> initialPositions = new Dictionary<Draggable, Vector3>();
-    private Dictionary<Transform, Draggable> snappedObjects = new Dictionary<Transform, Draggable>();
+    
 
     void Start()
     {
-        foreach (Transform snapPoint in snapPoints) {
-            snappedObjects.Add(snapPoint, null);
-
-        }
-
         foreach(Draggable draggable in draggableObjects) {
             initialPositions.Add(draggable, draggable.transform.position);
             draggable.dragEndedCallback = OnDragEnded;
-
-            foreach (Transform snapPoint in snapPoints) {
-                if (Vector3.Distance(draggable.transform.position, snapPoint.position) < snapRange) {
-                    snappedObjects[snapPoint] = draggable;
-                    break;
-                }
-            }
         }
-
+        
     }
 
     private void OnDragEnded(Draggable draggable) {
@@ -42,10 +30,8 @@ public class snapController : MonoBehaviour
             float currentDistance = Vector2.Distance(draggable.transform.localPosition, snapPoint.localPosition);
             
             if (currentDistance < closestDistance && currentDistance <= snapRange) {
-                if (snappedObjects[snapPoint] == null) {
-                    closestSnapPoint = snapPoint;
-                    closestDistance = currentDistance;
-                }
+                closestSnapPoint = snapPoint;
+                closestDistance = currentDistance;
 
             }
         }
@@ -53,7 +39,6 @@ public class snapController : MonoBehaviour
             if (closestSnapPoint != null) {
                 draggable.transform.localPosition = closestSnapPoint.localPosition;
                 initialPositions[draggable] = closestSnapPoint.position;
-                snappedObjects[closestSnapPoint] = draggable;
                 print("Snapped!");
 
             } else {
