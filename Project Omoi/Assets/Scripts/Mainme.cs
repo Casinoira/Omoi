1dbc2ff38f207f9ae8ebede2d94727d25116f07c
Ilovebreadj <jun137j@gmail.com>
2024-05-13 04:36:54 +0800

    UI Menu Test
    Testing Before

---
+++ Project Omoi/Assets/Scripts/Mainme.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Mainme : MonoBehaviour
+{
+	public void PlayGame()
+	{
+		SceneManager. LoadSceneAsync("World");
+	}
+}
+
