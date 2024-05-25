7529911f1d5eb199ea27c845804ab7ab394bc9b4
Casinoira <100073808@students.swinburne.edu.my>
2024-05-25 22:49:48 +0800

    Added Ink Tray for Pinball

--- Project Omoi/Assets/Scripts/Controls/snapController.cs
+++ Project Omoi/Assets/Scripts/Controls/snapController.cs
@@ -2,77 +2,106 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class snapController : MonoBehaviour
+public class SnapController : MonoBehaviour
 {
     public List<Transform> snapPoints;
     public List<Draggable> draggableObjects;
     public float snapRange = 0.5f;
+    public Collider2D trayCollider; // Reference to the tray collider
     private Dictionary<Draggable, Vector3> initialPositions = new Dictionary<Draggable, Vector3>();
     private Dictionary<Transform, Draggable> snappedObjects = new Dictionary<Transform, Draggable>();
 
     void Start()
     {
-        foreach (Transform snapPoint in snapPoints) {
+        foreach (Transform snapPoint in snapPoints)
+        {
             snappedObjects.Add(snapPoint, null);
-
         }
 
-        foreach(Draggable draggable in draggableObjects) {
+        foreach (Draggable draggable in draggableObjects)
+        {
             initialPositions.Add(draggable, draggable.transform.position);
             draggable.dragEndedCallback = OnDragEnded;
 
-            foreach (Transform snapPoint in snapPoints) {
-                if (Vector3.Distance(draggable.transform.position, snapPoint.position) < snapRange) {
+            foreach (Transform snapPoint in snapPoints)
+            {
+                if (Vector3.Distance(draggable.transform.position, snapPoint.position) < snapRange)
+                {
                     snappedObjects[snapPoint] = draggable;
                     break;
                 }
             }
         }
-
     }
+    private void OnDragEnded(Draggable draggable)
+    {
+        bool isInTray = IsInTray(draggable);
+        
+        if (isInTray)
+        {
+            // If the draggable is in the tray, place it at the release point within the tray
+            Vector3 releasePoint = draggable.transform.position;
+            releasePoint.x = Mathf.Clamp(releasePoint.x, trayCollider.bounds.min.x, trayCollider.bounds.max.x);
+            releasePoint.y = Mathf.Clamp(releasePoint.y, trayCollider.bounds.min.y, trayCollider.bounds.max.y);
+            releasePoint.z = 0f; // Assuming the tray is in 2D space
 
-    private void OnDragEnded(Draggable draggable) {
-        float closestDistance = Mathf.Infinity;
-        Transform closestSnapPoint = null;
-        Vector3 lastSnappedPosition = initialPositions[draggable];
-        // int index = draggableObjects.IndexOf(draggable);
-            
-        foreach(Transform snapPoint in snapPoints) {
-
-            float currentDistance = Vector2.Distance(draggable.transform.localPosition, snapPoint.localPosition);
-            
-            if (currentDistance < closestDistance && currentDistance <= snapRange && snappedObjects[snapPoint] == null) {
-                closestSnapPoint = snapPoint;
-                closestDistance = currentDistance;
-
-            }
+            draggable.transform.position = releasePoint;
+            Debug.Log("Placed in tray at " + releasePoint);
         }
+        else
+        {
+            // If the draggable is not in the tray, proceed with snapping logic
+            float closestDistance = Mathf.Infinity;
+            Transform closestSnapPoint = null;
+            Vector3 lastSnappedPosition = initialPositions[draggable];
 
-            if (closestSnapPoint != null) {
+            foreach (Transform snapPoint in snapPoints)
+            {
+                float currentDistance = Vector2.Distance(draggable.transform.localPosition, snapPoint.localPosition);
+
+                if (currentDistance < closestDistance && currentDistance <= snapRange && (snappedObjects[snapPoint] == null || snappedObjects[snapPoint] == draggable))
+                {
+                    closestSnapPoint = snapPoint;
+                    closestDistance = currentDistance;
+                }
+            }
+
+            if (closestSnapPoint != null)
+            {
                 ReleaseSnapPoint(draggable);
-                
                 draggable.transform.localPosition = closestSnapPoint.localPosition;
                 initialPositions[draggable] = closestSnapPoint.position;
                 snappedObjects[closestSnapPoint] = draggable;
-                print("Snapped!");
-
-            } else {
-                draggable.transform.position = lastSnappedPosition;
-                // draggable.transform.position = initialPositions[index];
-                print("Not Snapped");
+                Debug.Log("Snapped!");
             }
+            else
+            {
+                draggable.transform.position = lastSnappedPosition;
+                Debug.Log("Not Snapped");
+            }
+        }
+    }
 
+    private bool IsInTray(Draggable draggable)
+    {
+        return trayCollider.OverlapPoint(draggable.transform.position);
     }
 
     private void ReleaseSnapPoint(Draggable draggable)
     {
+        List<Transform> keysToRemove = new List<Transform>();
+
         foreach (var pair in snappedObjects)
         {
             if (pair.Value == draggable)
             {
-                snappedObjects[pair.Key] = null;
-                break;
+                keysToRemove.Add(pair.Key);
             }
+        }
+
+        foreach (var key in keysToRemove)
+        {
+            snappedObjects[key] = null;
         }
     }
 }
--- Project Omoi/Assets/Scripts/Controls/snapController_backup.cs
+++ Project Omoi/Assets/Scripts/Controls/snapController_backup.cs
@@ -2,21 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class snapController_backup : MonoBehaviour
+public class snapControllerBackup : MonoBehaviour
 {
     public List<Transform> snapPoints;
     public List<Draggable> draggableObjects;
     public float snapRange = 0.5f;
     private Dictionary<Draggable, Vector3> initialPositions = new Dictionary<Draggable, Vector3>();
-    
+    private Dictionary<Transform, Draggable> snappedObjects = new Dictionary<Transform, Draggable>();
 
     void Start()
     {
+        foreach (Transform snapPoint in snapPoints) {
+            snappedObjects.Add(snapPoint, null);
+
+        }
+
         foreach(Draggable draggable in draggableObjects) {
             initialPositions.Add(draggable, draggable.transform.position);
             draggable.dragEndedCallback = OnDragEnded;
+
+            foreach (Transform snapPoint in snapPoints) {
+                if (Vector3.Distance(draggable.transform.position, snapPoint.position) < snapRange) {
+                    snappedObjects[snapPoint] = draggable;
+                    break;
+                }
+            }
         }
-        
+
     }
 
     private void OnDragEnded(Draggable draggable) {
@@ -29,7 +41,7 @@ public class snapController_backup : MonoBehaviour
 
             float currentDistance = Vector2.Distance(draggable.transform.localPosition, snapPoint.localPosition);
             
-            if (currentDistance < closestDistance && currentDistance <= snapRange) {
+            if (currentDistance < closestDistance && currentDistance <= snapRange && snappedObjects[snapPoint] == null) {
                 closestSnapPoint = snapPoint;
                 closestDistance = currentDistance;
 
@@ -37,8 +49,11 @@ public class snapController_backup : MonoBehaviour
         }
 
             if (closestSnapPoint != null) {
+                ReleaseSnapPoint(draggable);
+                
                 draggable.transform.localPosition = closestSnapPoint.localPosition;
                 initialPositions[draggable] = closestSnapPoint.position;
+                snappedObjects[closestSnapPoint] = draggable;
                 print("Snapped!");
 
             } else {
@@ -49,4 +64,15 @@ public class snapController_backup : MonoBehaviour
 
     }
 
+    private void ReleaseSnapPoint(Draggable draggable)
+    {
+        foreach (var pair in snappedObjects)
+        {
+            if (pair.Value == draggable)
+            {
+                snappedObjects[pair.Key] = null;
+                break;
+            }
+        }
+    }
 }
